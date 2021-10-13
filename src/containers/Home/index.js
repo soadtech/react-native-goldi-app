ba26dbc99774eee47a189ce651963cfb7571f545
SoadTech <soadtech@MacBook-Air-de-SoadTech.local>
2021-10-12 20:00:00 -0500

    chore: cambiando color al statusBar

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import MyStatusBar from '../../components/MyStatusBar'
+import { colors } from '../../utils/theme'
 
 export default function Home () {
     return (
         <View>
-            <MyStatusBar backgroundColor="#5E8D48" barStyle="light-content" />
+            <MyStatusBar backgroundColor={colors.white} barStyle="dark-content" />
             <Text>Hola</Text>
         </View>
     )
